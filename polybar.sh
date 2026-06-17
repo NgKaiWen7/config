c8235876e52ca57b8bc28946adfad905a29dc99b
NgKaiWen7 <73570728+NgKaiWen7@users.noreply.github.com>
2026-06-17 08:31:14 +0800

    Refactor polybar script for improved execution
    Updated shebang to use env for portability and added logic to restart polybar on connected monitors.

--- polybar.sh
+++ polybar.sh
@@ -1,4 +1,12 @@
-#!/bin/bash
-killall -q polybar
-polybar main &
+#!/usr/bin/env bash
 
+polybar-msg cmd quit 2>/dev/null
+killall -q polybar
+
+while pgrep -x polybar >/dev/null; do
+    sleep 0.2
+done
+
+for monitor in $(xrandr --query | awk '$2 == "connected" {print $1}'); do
+    MONITOR="$monitor" polybar main -c ~/.config/polybar/config.ini &
+done
